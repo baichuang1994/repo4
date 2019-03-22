b36e0ab0c19ad6ad2925f3f7821de04f6eca35c2
bai <123@123.com>
2019-03-22 21:34:04 +0800

    把本地工程添加到git

---
+++ git-test/src/com/heima/FistClass.java
@@ -0,0 +1,13 @@
+package com.heima;
+
+public class FistClass {
+
+    public static void main(String[] args) {
+        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd");
+        System.out.println("ddddddddddd");
+    }
+}
